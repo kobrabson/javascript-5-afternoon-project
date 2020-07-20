4c3d8d63d2cf81635af93d63133194746800cc79
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-20 15:45:20 -0600

    prob 8

--- closures.js
+++ closures.js
@@ -222,7 +222,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
